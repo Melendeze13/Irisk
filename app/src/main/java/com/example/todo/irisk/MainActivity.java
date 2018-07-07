c9e48b91a0132e4d9df845a3a41d0c04a5f17578
Nithin Krishnamurthi <nithin.krishnamurthi401@gmail.com>
2018-07-06 21:42:43 -0700

    Imported OpenCV

--- app/src/main/java/com/example/todo/irisk/MainActivity.java
+++ app/src/main/java/com/example/todo/irisk/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.todo.irisk;
 
+import android.Manifest;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.objdetect.Objdetect;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int CAMERA_REQUEST = 1888;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[] {Manifest.permission.CAMERA}, CAMERA_REQUEST);
         setContentView(R.layout.activity_main);
     }
 }
